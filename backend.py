2e545b30d4682edb81135cbf9605c328417cb018
zerobug110 <mahabojang110@gmail.com>
2021-03-04 21:38:31 +0000

    python f formate

---
+++ backend.py
@@ -0,0 +1,15 @@
+course = 'python course'
+
+print(course[:5]);
+
+first = 'John'
+last = 'Smith'
+message = first + ' [' + last + '] is a coder'
+
+#formated string 
+msg = f'{first} [{last}] is a coder'
+
+print(message);
+print(msg);
+
+print(len(msg));
